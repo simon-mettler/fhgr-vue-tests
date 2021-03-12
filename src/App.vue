1d590a148a15e0537ee2532dd7be17eb4916dc51
Simon Mettler <admin@simonmettler.com>
2021-03-12 16:07:38 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/fhgr.jpg">
-    <HelloWorld msg="Hallo Team!"/>
+	<div class="container">
+		<div id="app">
+			<HelloWorld msg="Hallo Team!"/>
+		</div>
   </div>
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -15,12 +17,25 @@ export default {
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+	background: url(https://blog.fhgr.ch/wp-content/uploads/2018/07/bg-pattern-web_allgem.svg);
+  background-color: rgba(0, 0, 0, 0);
+  background-attachment: scroll;
+	background-attachment: fixed;
+	background-color: #f6f6f6;
+	margin: 0;
+}
+.container {
+	max-width: 900px;
+	border: 1px solid #ecece8;
+	margin: 80px auto;
+	background: white;
+	padding: 100px 20px;
+}
+p {
+	font-family: Roboto,sans-serif;
+	font-weight: 300;
+	font-style: normal;
 }
 </style>
